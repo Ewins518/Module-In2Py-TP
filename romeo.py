ec87c419f1f444fbf373f36378378d5844ef82e0
Ewins518 <74628423+Ewins518@users.noreply.github.com>
2021-06-17 17:01:31 +0200

    Update romeo.py

--- romeo.py
+++ romeo.py
@@ -5,3 +5,44 @@ moyenne_par_matiere = [ (romeo['SE'][0] + romeo['SE'][1] + romeo['SE'][2])/3,  (
 moyenne = (moyenne_par_matiere[0] + moyenne_par_matiere[2] + moyenne_par_matiere[3] + moyenne_par_matiere[4] + moyenne_par_matiere[5] + moyenne_par_matiere[6] + moyenne_par_matiere[1] + moyenne_par_matiere[7] + moyenne_par_matiere[8] + moyenne_par_matiere[9] )/len(moyenne_par_matiere)
 
 print(moyenne)
+
+#E0
+
+L =   [1,2,3,4,5,6,7,8,9,10]
+
+L[0] = L[0] + 11
+L[1] = L[1] + 11
+L[2] = L[2] + 11
+L[3] = L[3] + 11
+L[4] = L[4] + 11
+L[5] = L[5] + 11
+L[6] = L[6] + 11
+L[7] = L[7] + 11
+L[8] = L[8] + 11
+L[9] = L[9] + 11
+
+L.append(22)
+L.extend([23,24])
+
+L1 = L[0::2]
+print(L1)
+
+L2 = L[1::2]
+print(L2)
+
+#L.delete(3)
+
+print(L)
+
+#E1
+
+d = {'nom':'Dupuis', "prenom":'Jacque','age':30}
+d['prenom'] = 'Jacques'
+
+print(d.keys())
+print(d.values())
+print(d)
+
+phrase = d['prenom'] +' '+ d['nom'] +' ' +'a'+' '+ (str)(d['age'])+ ' '+ 'ans'
+
+print(phrase)
